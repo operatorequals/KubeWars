5adafd68807b07ffafae653590b2f8f08d9f1809
operatorequals <john.torakis@gmail.com>
2021-01-10 02:28:55 +0200

    Implemented status page at 8080

--- bot/main.py
+++ bot/main.py
@@ -6,6 +6,7 @@ import os
 import ipaddress
 import sys
 import time
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 def __target_list(target_list_str):
     targets = []
@@ -20,6 +21,8 @@ def __target_list(target_list_str):
 
     return targets
 
+# ===========
+
 with os.popen("hostname -i") as p:
     IPADDRESS = ipaddress.ip_address(p.read().strip())
 
@@ -33,6 +36,8 @@ TARGETLIST = os.environ.get('TARGETS', "%s/16" % IPADDRESS)
 TARGETS = __target_list(TARGETLIST)
 PORT = 7587
 REMAINING_HP = HP
+
+# ===========
 
 statuses = {
     100: "Spawned",
@@ -52,24 +57,24 @@ def die():
     log({"code":504})
     sys.exit(0)
 
+# ===========
 
 def await_shots():
     global REMAINING_HP
-    while REMAINING_HP > 0:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind(('0.0.0.0', PORT))
-            s.listen()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('0.0.0.0', PORT))
+        s.listen()
+        while REMAINING_HP > 0:
             conn, addr = s.accept()
             with conn:
                 received_damage = len(conn.recv(4096))
-                REMAINING_HP -= received_damage
-                log({"code":500, "by":addr[0], "damage": received_damage, "HP":REMAINING_HP})
+            REMAINING_HP -= received_damage
+            log({"code":500, "by":addr[0], "damage": received_damage, "HP":REMAINING_HP})
     die()
 
 
 def shoot():
     while(True):
-        # time.sleep()
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             ip = random.choice(TARGETS)
@@ -77,10 +82,30 @@ def shoot():
             s.connect((ip, PORT))
             s.sendall(b'B'*DAMAGE)
             
-            log({"code":200, "whom":ip, "damage":DAMAGE})
+            log({"code":200, "target":ip, "damage":DAMAGE})
         except:
-            log({"code":404, "whom":ip})
+            log({"code":404, "target":ip})
 
+# ===========
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.send_header("Server", "KubeWars")
+        self.end_headers()
+        self.wfile.write(
+            bytes(json.dumps(
+            {"HP":{"total":HP,"remaining": REMAINING_HP, "percent":int((REMAINING_HP/HP)*100) }}
+                ),
+            "utf8")
+        )
+
+    def log_request(self, code):
+    # to not interfere with Pod logs
+        pass
+
+# ===========
 
 if __name__ == '__main__':
     
@@ -88,8 +113,13 @@ if __name__ == '__main__':
     
     random.seed(SEED)
     
-    s = threading.Thread(target=shoot)
-    s.daemon = True
-    s.start()
+    shoot_thr = threading.Thread(target=shoot)
+    shoot_thr.daemon = True
+    shoot_thr.start()
+
+    status_srv = HTTPServer(('', 8080), MyServer)
+    status_srv_thr = threading.Thread(target=status_srv.serve_forever)
+    status_srv_thr.daemon = True
+    status_srv_thr.start()
 
     await_shots()
